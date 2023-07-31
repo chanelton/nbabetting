71ebea1a1454948701c10b1e9ee1f559e326ce9a
Elton Chan <you@example.com>
2023-07-31 00:31:40 -0700

    output table, toggle functionality

--- app.py
+++ app.py
@@ -1,65 +1,72 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+
 oddsdf = pd.read_csv('nbaodds.csv')
 
-@app.route('/filter_data')
+
+@app.route('/filter_data', methods=['POST'])
 def filter_data():
-    var_dict = {"bet_type": "type",
-                "season": "season",
-                "team":  "team",
+    key_dict = {"bet_type": "type",
                 "regular_or_playoffs": "reg_or_playoff",
                 "home_or_away": "team_home_away",
-                "opponent": "opponent",
-                "favorite": "favorite",
-                "bookmaker": "bookmaker",
-                "line": "line",
-                "odds": "odds",
                 "over_or_under": "over_under",
-                "hit": "hit"
+                "line": "line_float"
+                }
+    val_dict = {"favorite": 1,
+                "underdog": 0,
+                "regular season": "reg",
+                "playoffs": "playoff",
+                "hit": 1,
+                "miss": 0
                 }
     try:
-        print('balls')
         json_bet_data = request.get_json()
-        # json_bet_data = [
-        #     {
-        #         "bet": 1,
-        #         "bet_amount": "300",
-        #         "filters": {
-        #             "season": "2021-2022",
-        #             "bet_type": "spread",
-        #             "team": "76ers"
-        #             }
-        #     },
-        #     {
-        #         "bet": 2,
-        #         "bet_amount": "100",
-        #         "filters": {
-        #             "team": "76ers"
-        #             }
-        #     }
-        # ]
-        print(json_bet_data)
+        output_array = []
         for bet in json_bet_data:
             idx = bet["bet"]
             amount = float(bet["bet_amount"])
             filters = bet["filters"]
-            print(idx, amount, filters)
 
-            df = oddsdf
+            df = oddsdf.copy()
             for key, value in filters.items():
-                df = df[df[var_dict[key]] == value]
+                # if key in ["line", "odds", "over_or_under"]:
+                if key in ["line", "odds"]:
+                    limiters = [float(i) for i in value]
+                    smaller = min(limiters)
+                    larger = max(limiters)
+                    df = df[df[key_dict.get(key, key)].between(
+                        smaller, larger)]
+                elif key == "over_or_under":
+                    over_or_under = value[0]
+                    limiters = [float(i) for i in value[1:3]]
+                    smaller = min(limiters)
+                    larger = max(limiters)
+                    df = df[df[key_dict.get(key, key)] == over_or_under]
+                    df = df[df['line_float'].between(smaller, larger)]
+                else:
+                    df = df[df[key_dict.get(key, key)]
+                            == val_dict.get(value, value)]
 
+            num_games = len(df)
+            total_bet_amount = num_games * amount
             payout = round(sum(df['payout_multiplier'] * amount), 2)
-            print(payout)
+            profit = payout - total_bet_amount
+            bets_won = int(sum(df['hit']))
 
-        return 'hello'
+            output_array.append([idx] + [f"{i:,}" for i in [num_games, bets_won]
+                                         ] + [str(round(bets_won/num_games * 100, 2)) + '%'] + [f"{i:,.2f}" for i in [amount,
+                                total_bet_amount, payout, round(profit, 2), round(profit/num_games, 2), round(profit/total_bet_amount, 2)]])
+        return output_array
     except Exception as e:
         return jsonify({'error': str(e)})
     return 'data!'
